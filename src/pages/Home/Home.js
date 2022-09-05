a3c19df507a3cedd962c9d383f39fc65b5b4570b
Lucas Vinicius <Lucas.elias@gow solucoes.com>
2022-09-04 21:11:58 -0300

    dps continuo

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
 
     return (
         <div className={styles.container}>
-            <header>
+            <header className={styles.header}>
                 <div>
                     <h1>atumalaca</h1>
                 </div>
