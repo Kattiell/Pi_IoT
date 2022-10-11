3c00461f44c469b18db98bb0895f9ab6e6daba13
kattiel <gabrielcaetano@unipam.edu.br>
2022-10-11 13:21:12 -0300

    login,cadastrar e pag inicial

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import { Link } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login";
+import SingUp from "./pages/SignUp";
 
 export default function App() {
   return (
-    <div className="container">
-      <header className="header">
-        <div className="header-buttons">
-          <button>Cadastre-se</button>
-            
-          <button onClick={Login}>
-            <Link to="/Login">Login</Link>
-            </button>
-            
+    <div className="container2">
+      <div className="absoluta">
+        <div className="text">
+          <h1>AGROTECH</h1>
+          <h2>Soluções agrárias,</h2>
+          <h2>sempre ao lado de </h2>
+          <h2>quem produz</h2>
         </div>
-      </header>
+
+        <div className="header-buttons">
+        <button onClick={Login}>
+            <Link className="link" to="/Login">Login</Link>
+          </button>
+
+          <button onClick={SingUp}>
+            <Link className="link" to="/cadastre-se">Cadastre-se</Link>
+          </button>  
+        </div>
+      </div>
+      <div className="container"></div>
     </div>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,74 @@
+import React, { useState } from "react";
+import "./index1.css";
+
+import { MdEmail, MdLock, MdOutlinePeopleAlt, MdOutlineRoofing } from "react-icons/md";
+import { HiEye, HiEyeOff } from "react-icons/hi";
+
 export default function SignUp() {
-    return (
-        <div>
-            aqui tu se cadastra
+  const [email, setEmail] = useState("");
+  const [nome, setNome] = useState("");
+  const [nomef, setNomef] = useState("");
+  const [password, setPassword] = useState("");
+  const [show, setShow] = useState(false);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setShow(!show);
+  };
+
+  return (
+    <div className="SignUp">
+      <div className="SignUp-right">
+        <h1>Cadastre-se</h1>
+
+        {/* <img src="https://media.istockphoto.com/vectors/green-agro-icon-vector-id1167533705?k=20&m=1167533705&s=170667a&w=0&h=lxnygeEpIA8yQl0SCAtNF7uf8eBU4vKdg0VGMp3rR54="></img> */}
+
+        <div className="SignUp-SignUpInputEmail">
+          <MdOutlinePeopleAlt  />
+          <input
+            type="nome  "
+            placeholder="Informe seu nome completo"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          />
         </div>
-    );
+        <div className="SignUp-SignUpInputEmail">
+          <MdOutlineRoofing/>
+          <input
+            type="nomef"
+            placeholder="Informe o nome da sua fazenda"
+            value={nomef}
+            onChange={(e) => setNomef(e.target.value)}
+          />
+        </div>
+        <div className="SignUp-SignUpInputEmail">
+          <MdEmail />
+          <input
+            type="email"
+            placeholder="Informe seu e-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="SignUp-SignUpInputPassword">
+          <MdLock />
+          <input
+            placeholder="Informe sua senha"
+            type={show ? "text" : "password"}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className="SignUp-eye">
+            {show ? (
+              <HiEye size={20} onClick={handleClick} />
+            ) : (
+              <HiEyeOff size={20} onClick={handleClick} />
+            )}
+          </div>
+        </div>
+
+        <button type="submit">Criar Conta</button>
+      </div>
+    </div>
+  );
 }
